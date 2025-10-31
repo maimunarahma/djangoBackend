7201314736e6a9005a5227dc060d3c76dbdde4f7
Maimuna Rahman <maimunarahman30@gmail.com>
2025-11-01 02:45:33 +0600

    ok

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import JsonResponse
+def home(request):
+    return JsonResponse({"message": "Django backend is running!"})
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
